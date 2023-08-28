ebf90cdeacf63a0f2644e62792d5bab1779c6428
Vladimir Bezhenar <me@vbezhenar.com>
2023-08-28 23:38:05 +0600

    Simplify styles and scripts

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,19 +1,9 @@
-initDivHighlightCopyButton();
+addDivHighlightCopyButtons();
 
-function initDivHighlightCopyButton() {
+function addDivHighlightCopyButtons() {
   const divHighlightElements = document.querySelectorAll("div.highlight");
-  for (const element of divHighlightElements) {
-    element.addEventListener("mouseenter", handleDivHighlightMouseEnter);
-    element.addEventListener("mouseleave", handleDivHighlightMouseLeave);
-  }
-}
-
-function handleDivHighlightMouseEnter(event) {
-  const divHighlightElement = event.target;
-  let copyButtonElement = divHighlightElement.querySelector("button.copy");
-
-  if (copyButtonElement === null) {
-    copyButtonElement = document.createElement("button");
+  for (const divHighlightElement of divHighlightElements) {
+    const copyButtonElement = document.createElement("button");
     copyButtonElement.classList.add("copy");
     const image = document.createElement("img");
     image.src = "/assets/copy.svg?230802";
@@ -23,25 +13,13 @@ function handleDivHighlightMouseEnter(event) {
     copyButtonElement.appendChild(image);
     copyButtonElement.addEventListener("click", handleCopyButtonClick);
     divHighlightElement.appendChild(copyButtonElement);
-    forceReflow(copyButtonElement);
   }
-  copyButtonElement.classList.add("visible");
 }
 
-function handleDivHighlightMouseLeave(event) {
-  const divHighlightElement = event.target;
-  const copyButtonElement = divHighlightElement.querySelector("button.copy");
-  copyButtonElement.classList.remove("visible");
-}
-
-async function handleCopyButtonClick(event) {
+function handleCopyButtonClick(event) {
   const copyButtonElement = event.currentTarget;
   const divHighlightElement = copyButtonElement.parentElement;
   const preElement = divHighlightElement.querySelector("pre");
   const text = preElement.textContent;
   navigator.clipboard.writeText(text);
-}
-
-function forceReflow(element) {
-  element.offsetHeight;
 }
